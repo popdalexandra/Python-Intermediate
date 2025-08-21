1392de733ebc02db2887017126f5b1cb5b34f0f7
Pop Denisa Alexandra <denisa-alexandra.pop@vitesco.com>
2025-08-21 17:52:43 +0300

    finish

---
+++ Turtle Challenge - Turtle Race/main.py
@@ -0,0 +1,38 @@
+import random
+from turtle import Turtle, Screen
+
+
+screen = Screen()
+screen.setup(width = 500, height = 400)
+user_bet = screen.textinput (title = "Make your bet!", prompt="Choose the turtle's color (red, orange, yellow, green, blue, purple):")
+print(user_bet)
+
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_pos = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
+
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x = -230, y = y_pos[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"Winner! The {winning_color} turtle is the winner!")
+            else:
+                print(f"Loser! The {winning_color} turtle is the winner!")
+           
+        rand_distance = random.randint(0,10)
+        turtle.forward(rand_distance)
+
+screen.exitonclick()
